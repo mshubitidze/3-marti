72d15875b06aaabdd79b47587abb06341ee9d728
mshubitidze <59510085+mshubitidze@users.noreply.github.com>
2024-02-04 03:25:30 +0400

    apparently useSearchParams needs suspense?

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import { DesignPicker } from "./_components/design-picker";
 import { MessageInput } from "./_components/message-input";
 import { PendingButton } from "./_components/pending-button";
@@ -7,8 +8,12 @@ export default async function IndexPage() {
   return (
     <main className="container mx-auto flex min-h-[calc(100svh-9rem)] flex-col justify-around">
       <form action={handleSubmit} className="-mt-8 space-y-6">
-        <DesignPicker />
-        <MessageInput />
+        <Suspense fallback={null}>
+          <DesignPicker />
+        </Suspense>
+        <Suspense fallback={null}>
+          <MessageInput />
+        </Suspense>
         <PendingButton />
       </form>
     </main>
